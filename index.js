72a28e3eacad24d6a44ac5caec07b33d589b31f7
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-26 17:56:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-function displayHighScoresJSON() {
-    var requestLocal = new XMLHttpRequest();
+var requestLocal = new XMLHttpRequest();
     
-    requestLocal.open("GET", "resultEpisodesList.json", false);
-    requestLocal.setRequestHeader("Content-Type", "application/json");
-    requestLocal.send();
+requestLocal.open("GET", "resultEpisodesList.json", false);
+requestLocal.setRequestHeader("Content-Type", "application/json");
+requestLocal.send();
 
-   var highScoresDiv = document.getElementById("items");
-   var json = JSON.parse(requestLocal.responseText);
+//var highScoresDiv = document.getElementById("items");
+var json = JSON.parse(requestLocal.responseText);
    
-   highScoresDiv.innerHTML = json.items[0];
-}
+// find elements
+var dataContainer = $("#data-container");
+var btn = $("#btn1");
 
-displayHighScoresJSON();
+// handle click and add class
+btn.on("click", function(e) {
+
+ajax({
+    url: "https://raw.githubusercontent.com/alv2017/DataSets/master/Europe/europe-capital-cities.json",
+    dataType: "json"
+  }).done(function(result) {
+    let id = Math.floor(Math.random() * 44);
+    let country = result['European Capitals'][id]['capital_name'];
+    let latitude = result['European Capitals'][id]['latitude'];
+    let longitude = result['European Capitals'][id]['longitude'];
+
+    let dstring = "Country name: " + country + ", Latitude: " + latitude + ", Longitude: " + longitude;
+    dataContainer.text(dstring);
+  });
+
+});

281a5d8c339bd60f0772633fbfb76c59d76265bf
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-26 18:19:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,7 @@ requestLocal.send();
 //var highScoresDiv = document.getElementById("items");
 var json = JSON.parse(requestLocal.responseText);
    
-// find elements
-var dataContainer = $("#data-container");
-var btn = $("#btn1");
+var okay = json.items;
+var ahh = "why yes!";
 
-// handle click and add class
-btn.on("click", function(e) {
-
-ajax({
-    url: "https://raw.githubusercontent.com/alv2017/DataSets/master/Europe/europe-capital-cities.json",
-    dataType: "json"
-  }).done(function(result) {
-    let id = Math.floor(Math.random() * 44);
-    let country = result['European Capitals'][id]['capital_name'];
-    let latitude = result['European Capitals'][id]['latitude'];
-    let longitude = result['European Capitals'][id]['longitude'];
-
-    let dstring = "Country name: " + country + ", Latitude: " + latitude + ", Longitude: " + longitude;
-    dataContainer.text(dstring);
-  });
-
-});
+document.getElementById("hmm").innerHTML = ahh;

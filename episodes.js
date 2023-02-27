1b07f4634c49ec652c0bb163ce8c3ca134351101
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-27 02:26:02 -0500

    Update episodes.js

--- episodes.js
+++ episodes.js
@@ -4,11 +4,7 @@ fetch('./data.json')
   .then((response) => response.json())
   .then((json) => JSON.parse(json))
   .then((myObj) => {
-    for (const ep of myObj.items) {
-      const listItem = document.createElement('wrap');
-      listItem.appendChild(document.createElement('tile'));
-      listItem.appendChild(document.createElement('text')).textContent = ep.name;
-    //document.getElementById("tile1title").innerHTML = myObj.items[0].name;
-    //document.getElementById("tile1desc").innerHTML = myObj.items[0].description.substring(0,200) + "...";
+    document.getElementById("tile1title").innerHTML = myObj.items[0].name;
+    document.getElementById("tile1desc").innerHTML = myObj.items[0].description.substring(0,200) + "...";
   
 });

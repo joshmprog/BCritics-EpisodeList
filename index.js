ef0da4d1337c4705cabb66b499709b16870524bd
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-26 21:02:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ fetch('./data.json')
   .then((response) => response.json())
   .then((json) => JSON.parse(json))
   .then((myObj) => {
-    console.log(myObj);
-    console.log(myObj.items[0].description);
+    //console.log(myObj);
+    //console.log(myObj.items[0].description);
+    document.getElementById("latestPod").innerHTML = myObj.items[0].name.substring(0,2);
+    document.getElementById("latestEmbed").src = "https://open.spotify.com/embed/track/" + myObj.items[0].id;
 });
 
 //const myOb = JSON.parse(myJSON);
 //console.log(myOb.items[0].description);
-
-   
-var ahh = "why yes!";
-
-document.getElementById("hmm").innerHTML = ahh;
 
 
 

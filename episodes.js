13383014241c8cea21e4a1b9ba98e444fcfac90d
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-27 00:10:57 -0500

    Create episodes.js

---
+++ episodes.js
@@ -0,0 +1,13 @@
+<!--./index.js-->
+
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((json) => JSON.parse(json))
+  .then((myObj) => {
+    //console.log(myObj);
+    //console.log(myObj.items[0].description);
+    document.getElementById("tile1num").innerHTML = myObj.items[0].name.substring(0,3).replace(/\D/g,'');
+    document.getElementById("tile1title").innerHTML = myObj.items[0].name;
+    document.getElementById("tile1desc").innerHTML = myObj.items[0].description;
+  
+});

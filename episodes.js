4691c088fca59cdb7fb25def475a5682d57af62a
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-27 11:36:42 -0500

    Update episodes.js

--- episodes.js
+++ episodes.js
@@ -3,9 +3,26 @@
 fetch('./data.json')
   .then((response) => response.json())
   .then((json) => JSON.parse(json))
-  .then((myObj) => {
-    for (var i = 1; i < 5; i++) { //data.tennis.length; i++) {
-    document.getElementById("tile" + i + "title").innerHTML = myObj.items[i-1].name;
-    document.getElementById("tile" + i + "desc").innerHTML = myObj.items[i-1].description.substring(0,200) + "...";
-    }
+  .then((data) => {
+      for (var i = 0; i < data.items.length; i++) {
+        var title = data.items[i].name;
+        var desc = data.items[i].description.substring(0,200) + "...";
+        var imageurl = data.items[i].images[0].url;
+        var key = data.items[i].id;
+        var badge = document.createElement('div');
+        badge.className = 'tile';
+        badge.id = key
+        badge.innerHTML =
+          '<img src="https://i.scdn.co/image/ab67656300005f1f7a26bdaa44ed8cad4cbac373"/>' +
+          '<div class="text">' +
+          '<h2>' + title + '</h2>' + 
+          '<p class="animate-text">' + desc + '</p>' + 
+          '<div class="dots">' + 
+            '<span></span>' + 
+            '<span></span>' + 
+            '<span></span>' + 
+          '</div>' + 
+          '</div>';
+        document.getElementById('badge-list').appendChild(badge);
+      }
 });

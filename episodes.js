890a73072c3e53e4280c34c5c3d98765a7797e12
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-27 01:55:17 -0500

    Update episodes.js

--- episodes.js
+++ episodes.js
@@ -8,6 +8,6 @@ fetch('./data.json')
     //console.log(myObj.items[0].description);
     //document.getElementById("tile1num").innerHTML = myObj.items[0].name.substring(0,3).replace(/\D/g,'');
     document.getElementById("tile1title").innerHTML = myObj.items[0].name;
-    document.getElementById("tile1desc").innerHTML = myObj.items[0].description;
+    document.getElementById("tile1desc").innerHTML = myObj.items[0].description.substring(0,100) + "...";
   
 });

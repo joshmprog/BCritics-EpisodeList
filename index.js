bfd5165b565cea64b1560730e2eecf598f87e01b
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-26 19:39:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 <!--./index.js-->
 
 fetch('./data.json')
-  .then((response) => response.json())
-  .then((data) => {
-	console.log(JSON.stringify(data));
-	console.log(data.items);	
-});
+    .then((response) => response.json())
+    .then((json) => const myJSON = json);
+
+const myOb = JSON.parse(myJSON);
+console.log(myOb.items[0].description);
 
    
 var ahh = "why yes!";

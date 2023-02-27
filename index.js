e04ef1c62ef6a2aa262984b67ee01d1139968865
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-26 19:55:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 <!--./index.js-->
 
-fetch('./data.json')
-    .then((response) => response.json())
-    .then((json) => const myJSON = json);
+const loadPosts = async () => {
+  const response = await fetch('./data.json');
+  const posts = await response.json();
+  return posts;
+};
 
-const myOb = JSON.parse(myJSON);
-console.log(myOb.items[0].description);
+const posts = await loadPosts();
+console.log(posts);
+
+//    .then((response) => response.json())
+//    .then((json) => const myJSON = json);
+
+//const myOb = JSON.parse(myJSON);
+//console.log(myOb.items[0].description);
 
    
 var ahh = "why yes!";

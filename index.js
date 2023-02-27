eafda8ee6f04f01e7e41c7a59bf6eca5267664b4
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-26 19:20:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 <!--./index.js-->
 
-const response = fetch('./data.json');
-const json = response.then(response.json());
+const okay = fetch('./data.json').then(function (response) {
+	// If the response is successful, get the JSON
+	if (response.ok) {
+		return response.json();
+	}
 
-console.log(JSON.stringify(json));
-console.log(json.items);
+console.log(JSON.stringify(okay));
+console.log(okay.items);
    
 var ahh = "why yes!";
 

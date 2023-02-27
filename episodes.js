ad1cd0299257305c19ff65bad6d069bf80fe143d
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-02-27 02:23:49 -0500

    Update episodes.js

--- episodes.js
+++ episodes.js
@@ -4,10 +4,11 @@ fetch('./data.json')
   .then((response) => response.json())
   .then((json) => JSON.parse(json))
   .then((myObj) => {
-    //console.log(myObj);
-    //console.log(myObj.items[0].description);
-    //document.getElementById("tile1num").innerHTML = myObj.items[0].name.substring(0,3).replace(/\D/g,'');
-    document.getElementById("tile1title").innerHTML = myObj.items[0].name;
-    document.getElementById("tile1desc").innerHTML = myObj.items[0].description.substring(0,200) + "...";
+    for (const ep of myObj.items) {
+      const listItem = document.createElement('wrap');
+      listItem.appendChild(document.createElement('tile'));
+      listItem.appendChild(document.createElement('text')).textContent = ep.name;
+    //document.getElementById("tile1title").innerHTML = myObj.items[0].name;
+    //document.getElementById("tile1desc").innerHTML = myObj.items[0].description.substring(0,200) + "...";
   
 });

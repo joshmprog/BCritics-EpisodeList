65e796b5547586d1a1058d2526612b09e67dcaee
joshmprog <112892731+joshmprog@users.noreply.github.com>
2023-03-02 01:25:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 <!--./index.js-->
 
-fetch('./data.json')
+fetch('datastore__data.json')
   .then((response) => response.json())
   .then((json) => JSON.parse(json))
   .then((myObj) => {
     //console.log(myObj);
     //console.log(myObj.items[0].description);
-    document.getElementById("latestPod").innerHTML = "Ep. 0" + myObj.items[0].name.substring(0,2) + "-<br>"+myObj.items[0].name.substring(5);
+    // document.getElementById("latestPod").innerHTML = "Ep. 0" + myObj.items[0].name.substring(0,2) + "-<br>"+myObj.items[0].name.substring(5);
     document.getElementById("latestEmbed").src = "https://open.spotify.com/embed/episode/" + myObj.items[0].id;
+  
 });
 
-//const myOb = JSON.parse(myJSON);
-//console.log(myOb.items[0].description);
-
+/*Only needed for the controls*/
+var phone = document.getElementById("phone_1"),
+  iframe = document.getElementById("frame_1");
 
 
